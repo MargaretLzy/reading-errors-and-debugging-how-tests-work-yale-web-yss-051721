3ee5aff787317b2fe562786069c800252c8b1e06
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-29 20:07:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 
-second_number = 6
+first_number = 6
+second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
